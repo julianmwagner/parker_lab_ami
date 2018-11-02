15547d8f2b72c85c08edc342f41ea9f004531e32
julianmwagner <jwagner2@caltech.edu>
2018-11-02 12:01:34 -0700

    Update setup_ami.sh

--- setup_ami.sh
+++ setup_ami.sh
@@ -18,9 +18,34 @@ jupyter labextension install --no-build @pyviz/jupyterlab_pyviz;
 jupyter lab build;
 echo 'export PS1="\[\e[1;32m\]\u\[\e[0m\]@\e[1;36m\]\h\[\e[0m\] [\w]\n% "' >> ~/.bashrc;
 echo 'export LSCOLORS="gxfxcxdxCxegedabagacad"' >> ~/.bashrc;
+
+#Install R
 sudo bash -c "echo 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/' >> /etc/apt/sources.list"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo apt-get update
 sudo apt-get install libopenblas-base r-base
 sudo apt-get install r-base-dev
 sudo apt-get install libcurl4-openssl-dev libssl-dev
+
+#Kallisto
+conda create -n kallisto_env
+source activate kallisto_env
+conda install -y -c bioconda kallisto
+source deactivate
+echo 'alias kallisto="$HOME/miniconda/envs/kallisto_env/bin/kallisto"' >> ~/.bashrc;
+
+#MrBayes
+sudo apt-get install mrbayes;
+echo 'alias mb="/usr/bin/mb"' >> ~/.bashrc;
+
+#BLAST
+sudo apt-get install ncbi-blast+;
+
+#Trinity and Trinotate
+conda create -y -n trinity_env;
+source activate trinity_env;
+conda install -y -c bioconda trinity trinotate;
+echo 'alias Trinity="$HOME/miniconda/envs/trinity_env/bin/Trinity"' >> ~/.bashrc;
+echo 'alias Trinotate="$HOME/miniconda/envs/trinity_env/bin/Trinotate"' >> ~/.bashrc;
+
+source ~/.bashrc;
